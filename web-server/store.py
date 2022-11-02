3c26164217421de69289c23b5119c4de20aebf2f
Jorge Garcia <alberto-garcia20@outlook.com>
2022-11-02 14:01:59 -0600

    Usando la libreria requests de python

---
+++ web-server/store.py
@@ -0,0 +1,13 @@
+import requests
+
+
+def get_categories():
+    r = requests.get('https://api.escuelajs.co/api/v1/categories')
+    print(r.status_code)
+    # print(r.text)
+    categories = r.json()
+    # print(type(categories))
+    # print(categories['name'])
+
+    for category in categories:
+        print(category['name'])
