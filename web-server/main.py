e9ff7f4bb6ac850e5f3f191289b16f78f391e23b
Jorge Garcia <alberto-garcia20@outlook.com>
2022-11-03 15:39:11 -0600

    Usando FastAPI para crear un pequeño servicio web

--- web-server/main.py
+++ web-server/main.py
@@ -1,4 +1,42 @@
 import store
+from fastapi import FastAPI
+from fastapi.responses import HTMLResponse
+
+
+app = FastAPI()
+
+
+@app.get('/')
+def get_list():
+    return [
+        {
+            'name': 'Jorge',
+            'last_name': 'Garcia',
+            'age': 23,
+            'email': 'jorge@something.com'
+        },
+        {
+            'name': 'Alberto',
+            'last_name': 'Estrada',
+            'age': 24,
+            'email': 'albert-estrada@something.com'
+        },
+        {
+            'name': 'Maria',
+            'last_name': 'Iniesta',
+            'age': 20,
+            'email': 'mary-I@something.com'
+        }
+    ]
+
+
+@app.get('/contact/', response_class=HTMLResponse)
+def get_contact():
+    return """
+        <h1>Respuesta HTML desde FastAPI</h1>
+        <h2>Elaborado por Jorge Garcia <3</h2>
+        <p>Esto es un párrafo</p>
+    """
 
 
 def run():
--- web-server/store.py
+++ web-server/store.py
@@ -2,12 +2,14 @@ import requests
 
 
 def get_categories():
-    r = requests.get('https://api.escuelajs.co/api/v1/categories')
+    # r = requests.get('https://api.escuelajs.co/api/v1/categories')
+    r = requests.get('http://127.0.0.1:8000/')
     print(r.status_code)
     # print(r.text)
     categories = r.json()
+    print(categories)
     # print(type(categories))
     # print(categories['name'])
 
-    for category in categories:
-        print(category['name'])
+    # for category in categories:
+    #     print(category['name'])
